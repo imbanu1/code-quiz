5a20e381878e8674096940426da9e39cf9d5bbf4
ike imbanu <imbanu1@gmail.com>
2023-11-29 19:02:47 -0400

    Made changes to my index and css files

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+var questionsInfo = [
+question1 = {
+    question:"Which of these is not a comparison operator in JavaScript?",
+    multipleoptions: ["==", "!=", "===", "*", "!=="],
+    corerct: "=="
+
+},
+{
+
+    question: "Which one of these is a primitive data type?",
+    multipleoptions: ["Arrays", "Objects", "Boolean", "Classes", "Styles"],
+},
+
+{
+
+    question: "Select one of the following components that is not part of a box model.",
+    multipleoptions: ["Margin", "Padding", "Content", "Element", "Border"],
+},
+
+{
+
+    question: "Among the following element types commonly used to add content to pages, which one is typically not used for this purpose?",
+    multipleoptions: ["Font element", "Text element", "Image element", "Head element", "List element"],
+},
+{
+
+    question: "Which one of these is not part of the Command Line Interface directory?",
+    multipleoptions: ["pwd", "Ls", "mkdir", "Cd..", "Shell"],
+},
+
+
+];
+
+let hello = document.getElementById('hello');
+
+
+hello.append('Ho Ho Ho')
+//create a function generates this array and appends its values on the htrml
+
+//you can add a  display:hidden or display:visible css onto this div
+//css .hidden{ display:hidden}
+
+//classList.add('hidden)
+//classList.remove('hidden')
